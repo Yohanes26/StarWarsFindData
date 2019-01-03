cfa9689c826c56a80207e4851eb4bed1488306c8
Yohanes26 <yohaneslopes@gmail.com>
2019-01-03 21:50:13 -0200

    Inclusao dos componentes de arquitetura, juntamente com os testes unitarios

--- src/app/layout/footer/footer.module.ts
+++ src/app/layout/footer/footer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FooterComponent]
 })
 export class FooterModule { }
