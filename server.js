7d536cd5f09eec5bd43acf763e512621ae60aa88
Yohanes26 <yohaneslopes@gmail.com>
2019-01-03 20:26:45 -0200

    Inclusao do arquivo que o node utiliza para parametrizar o server inclusive onde se localiza os arquivos dist

---
+++ server.js
@@ -0,0 +1,14 @@
+//Install express server
+const express = require('express');
+const app = express();
+var path = require('path');
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/star-wars-find-data'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
